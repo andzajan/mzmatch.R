eb355fde293c41e9f0e70ebe5e436fe65ef02053
9527567 <50236967+9527567@users.noreply.github.com>
2020-11-21 11:03:09 +0800

    Update PeakML.Viewer.R

--- R/PeakML.Viewer.R
+++ R/PeakML.Viewer.R
@@ -79,13 +79,16 @@ PeakML.Viewer <- function(arch="detect",install.path=NULL, JHeapSize=1024, unins
 	{
 		java <- "java"
 	}
-	
+	if(arch == "Linux_64")
+	{
+		java<- "java"
+	}
 	if (arch=="OSX_64")
 	{
 		java <- paste(java, " -XstartOnFirstThread",sep="")
 	}
 	
-	start.command <- paste("java"," -Xms",JHeapSize,"m -Xmx",JHeapSize,"m -jar ",viewerfile_short,sep="")
+	start.command <- paste(java," -Xms",JHeapSize,"m -Xmx",JHeapSize,"m -jar ",viewerfile_short,sep="")
 	
 	## Temporary change working folder to PeakMl viewer path, to load also proper settings.xml file. Satr viewer and change folder back.
 	if (uninstall==FALSE)
